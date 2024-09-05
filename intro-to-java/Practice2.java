ec0a215052a30e644d6c45dfcec7238173e41924
XIAO0101-star <70090234+XIAO0101-star@users.noreply.github.com>
2024-09-05 06:31:28 +0000

    Practice 2 add 2.3

--- intro-to-java/Practice2.java
+++ intro-to-java/Practice2.java
@@ -9,30 +9,67 @@ public class Practice2 {
                 break;
             }
             else if ( shape.equals("rectangle") ){
-                rectangle r1 = new rectangle();
-                System.out.println("Please enter the top-left coordinate, length and width of the rectangle: ");
-                System.out.println("Now enter x-axis coordinate, hit enter key when finish input: ");
-                r1.setX(sc.nextDouble());
-                System.out.println("Now enter y-axis coordinate, hit enter when finish: ");
-                r1.setY(sc.nextDouble());
                 while (true){
-                    System.out.println("Now enter length of the rectangle, hit enter when finish: ");
-                    r1.setLength(sc.nextDouble());
-                    if (r1.getLength() != 0){
+                    rectangle r1 = new rectangle();
+                    System.out.println("Please enter the top-left coordinate, length and width of the rectangle: ");
+                    System.out.println("Now enter x-axis coordinate, hit enter key when finish input: ");
+                    r1.setX(sc.nextDouble());
+                    System.out.println("Now enter y-axis coordinate, hit enter when finish: ");
+                    r1.setY(sc.nextDouble());
+                    while (true){
+                        System.out.println("Now enter length of the rectangle, hit enter when finish: ");
+                        r1.setLength(sc.nextDouble());
+                        if (r1.getLength() != 0){
+                            break;
+                        }
+                    }
+                    while (true){
+                        System.out.println("Now enter the width of the rectangle, hit enter when finish: ");
+                        r1.setWidth(sc.nextDouble());
+                        if (r1.getWidth() != 0){
+                            break;
+                        }
+                    }
+
+                    System.out.println("The top-left coordinate of the rectangle is: (" + r1.getX() + "," + r1.getY() + ")");
+                    System.out.println("The top-right coordinate of the rectangle is: (" + (r1.getX()+r1.getLength()) + "," + r1.getY() + ")");
+                    System.out.println("The bottom-left coordinate of the rectangle is: (" + r1.getX() + "," + (r1.getY()-r1.getWidth()) + ")");
+                    System.out.println("The bottom-right coordinate of the rectangle is: (" + (r1.getX()+r1.getLength()) + "," + (r1.getY()-r1.getWidth()) + ")");
+                    System.out.println("The area of the rectangle is: " + r1.getArea());
+                    sc.nextLine();
+                    System.out.println("Do you want to modify your rectangle? (y/n) ");
+                    String modifyr = sc.nextLine();
+                    if(modifyr.equals("n")){
                         break;
                     }
                 }
+            } 
+            else if (shape.equals("circle")){
                 while (true){
-                    System.out.println("Now enter the width of the rectangle, hit enter when finish: ");
-                    r1.setWidth(sc.nextDouble());
-                    if (r1.getWidth() != 0){
+                    circle c1 = new circle();
+                    System.out.println("Please input the coordination of center point and radius of the circle.");
+                    System.out.println("Now enter x-axis coordinate, hit enter key when finish input: ");
+                    c1.setX(sc.nextDouble());
+                    System.out.println("Now enter y-axis coordinate, hit enter when finish: ");
+                    c1.setY(sc.nextDouble());
+                    while (true){
+                        System.out.println("Now enter the radius of the circle, hit enter when finish: ");
+                        c1.setRadius(sc.nextDouble());
+                        if (c1.getRadius() != 0){
+                            break;
+                        }
+                    }
+
+                    System.out.println("The center point coordinate of the circle is: (" + c1.getX() + "," + c1.getY() + ")");
+                    System.out.println("The radius of the circle is: " + c1.getRadius());
+                    System.out.println("The area of the circle is: " + c1.getArea());
+                    System.out.println("Do you want to modify your circle? (y/n) ");
+                    sc.nextLine();
+                    String modifyc = sc.nextLine();
+                    if (modifyc.equals("n")){
                         break;
                     }
                 }
-                System.out.println("The top-left coordinate of the rectangle is: (" + r1.getX() + "," + r1.getY() + ")");
-                System.out.println("The top-right coordinate of the rectangle is: (" + (r1.getX()+r1.getLength()) + "," + r1.getY() + ")");
-                System.out.println("The bottom-left coordinate of the rectangle is: (" + r1.getX() + "," + (r1.getY()-r1.getWidth()) + ")");
-                System.out.println("The bottom-right coordinate of the rectangle is: (" + (r1.getX()+r1.getLength()) + "," + (r1.getY()-r1.getWidth()) + ")");
             } else {
                 System.out.println("Drawing of this shape is still under development. Please wait for further updates.");
             }
@@ -77,4 +114,38 @@ class rectangle{
                 System.out.println("Invalid input. Please try again. ");
             }
         }
+        public double getArea () {
+            return this.length * this.width;
+        }
+}
+
+class circle{
+    private double x;
+    private double y;
+    private double radius;
+    public double getX(){
+        return this.x;
+    }
+    public double getY(){
+        return this.y;
+    }
+    public double getRadius(){
+        return this.radius;
+    }
+    public void setX(double newX) {
+        this.x = newX;
+    }
+    public void setY(double newY) {
+        this.y = newY;
+    }
+    public void setRadius(double newRadius){
+        if (newRadius > 0){
+            this.radius = newRadius;
+        } else {
+            System.out.println("Invalid input. Please try again. ");
+        }
+    }
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
 }
